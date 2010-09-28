2ddff23dcdfd558869b90cc45271178d88803bc0
nadlabak <pavel@doshaska.net>
2010-09-28 06:01:25 -0700

    initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,79 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/motorola/umts_sholes/BoardConfigVendor.mk
+
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RECOVERY := true
+TARGET_NO_KERNEL := true
+TARGET_NO_RADIOIMAGE := true
+
+TARGET_BOARD_PLATFORM := omap3
+
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_GLOBAL_CFLAGS += -mtune=cortex-a8
+TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a8
+
+TARGET_BOOTLOADER_BOARD_NAME := sholes
+
+# Wifi related defines
+BOARD_WPA_SUPPLICANT_DRIVER := CUSTOM
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := libCustomWifi
+WPA_SUPPLICANT_VERSION      := VER_0_6_X
+BOARD_WLAN_DEVICE           := tiwlan0
+WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/tiwlan_drv.ko"
+BOARD_WLAN_TI_STA_DK_ROOT   := system/wlan/ti/wilink_6_1
+WIFI_DRIVER_MODULE_ARG      := ""
+WIFI_DRIVER_MODULE_NAME     := "tiwlan_drv"
+WIFI_FIRMWARE_LOADER        := "wlan_loader"
+
+BOARD_USES_GENERIC_AUDIO := false
+BOARD_USES_ECLAIR_LIBCAMERA := true
+BOARD_USES_ECLAIR_LIBAUDIO := true
+
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+BOARD_EGL_CFG := device/motorola/umts_sholes/egl.cfg
+
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00380000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00480000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x0afa0000   # limited so we enforce room to grow
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x0c520000
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+BOARD_HAS_SMALL_RECOVERY := true
+
+TARGET_RECOVERY_UI_LIB := librecovery_ui_sholes librecovery_ui_generic
+
+TARGET_RECOVERY_UPDATER_LIBS += librecovery_updater_generic
+
+USE_SHOLES_PROPERTY := true
+
---
+++ umts_sholes.mk
@@ -0,0 +1,98 @@
+#
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# This is the product configuration for a generic GSM passion,
+# not specialized for any geography.
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_eu_supl.mk)
+
+## (1) First, the most specific values, i.e. the aspects that are specific to GSM
+
+PRODUCT_COPY_FILES += \
+    device/motorola/umts_sholes/init.mapphone_umts.rc:root/init.mapphone_umts.rc
+
+## (2) Also get non-open-source GSM-specific aspects if available
+$(call inherit-product-if-exists, vendor/motorola/umts_sholes/umts_sholes-vendor.mk)
+
+## (3)  Finally, the least specific parts, i.e. the non-GSM-specific aspects
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.com.android.wifi-watchlist=GoogleGuest \
+	ro.error.receiver.system.apps=com.google.android.feedback \
+	ro.setupwizard.enterprise_mode=1 \
+        ro.com.google.clientid=android-motorola \
+	ro.com.google.clientidbase=android-motorola \
+	ro.com.google.locationfeatures=1 \
+	ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+	ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+	ro.config.vc_call_vol_steps=7 \
+	ro.telephony.call_ring.multiple=false \
+	ro.telephony.call_ring.delay=3000 \
+	ro.url.safetylegal=http://www.motorola.com/staticfiles/Support/legal/?model=A855 \
+	ro.setupwizard.enable_bypass=1 \
+	ro.media.dec.jpeg.memcap=20000000 \
+	dalvik.vm.lockprof.threshold=500 \
+	dalvik.vm.dexopt-flags=m=y \
+        ro.product.multi_touch_enabled=true \
+        ro.product.max_num_touch=2 \
+        ro.com.google.gmsversion=2.2_r2 
+
+DEVICE_PACKAGE_OVERLAYS += device/motorola/umts_sholes/overlay
+
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml
+
+# media config xml file
+PRODUCT_COPY_FILES += \
+    device/motorola/umts_sholes/media_profiles.xml:system/etc/media_profiles.xml
+
+PRODUCT_PACKAGES += \
+    librs_jni
+
+# we have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+# Passion uses high-density artwork where available
+PRODUCT_LOCALES += hdpi
+
+PRODUCT_COPY_FILES += \
+    device/motorola/umts_sholes/vold.fstab:system/etc/vold.fstab
+
+
+$(call inherit-product-if-exists, vendor/motorola/umts_sholes/umts_sholes-vendor.mk)
+
+# media profiles and capabilities spec
+# $(call inherit-product, device/motorola/sholes/media_a1026.mk)
+
+# stuff common to all HTC phones
+#$(call inherit-product, device/htc/common/common.mk)
+
+$(call inherit-product, build/target/product/full.mk)
+
+
+PRODUCT_NAME := umts_sholes
+PRODUCT_DEVICE := umts_sholes
