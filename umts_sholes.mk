a31aa885494bf077eab15be42a2f8c7029b08ddc
nadlabak <pavel@doshaska.net>
2011-07-12 16:33:06 +0200

    max_num_touch 4, vm min_free_kbytes 4096
    Change-Id: Ib8f8cbe91269a37ebd78721e131b2fb32ec4cfce

--- umts_sholes.mk
+++ umts_sholes.mk
@@ -44,7 +44,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	dalvik.vm.lockprof.threshold=500 \
 	dalvik.vm.dexopt-flags=m=y \
         ro.product.multi_touch_enabled=true \
-        ro.product.max_num_touch=2 \
+        ro.product.max_num_touch=4 \
         ro.com.google.gmsversion=2.2_r9 
 
 # Enable TI HotSpot
