87ed6c90320a613b2fb225d3ad58c288ba9c3d59
nadlabak <pavel@doshaska.net>
2011-12-24 02:23:03 +0100

    use init built from source
    Change-Id: I799f22734efdac64bbda87c8d557a01e3bd27fd0

--- prebuilt/bin/sh_hijack.sh
+++ prebuilt/bin/sh_hijack.sh
@@ -7,5 +7,7 @@
    /system/xbin/rm -f /init_prep_keypad.sh
    /system/xbin/cp -fr /etc/rootfs/* /
    /system/xbin/chmod 750 /sbin/*
+   /system/xbin/chmod 750 /init
+   /system/xbin/ln -s /init /sbin/ueventd
 
    exec /system/bin/2nd-init
