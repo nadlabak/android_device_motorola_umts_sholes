f13ca4a51f9ed4d2335de7d02a6da5525e881640
nadlabak <pavel@doshaska.net>
2012-01-02 02:21:30 +0100

    fix mobile data, gps, market downloads...
    Change-Id: I91fdf8a3151dd1dfbf7dae61f130286beb0d1d85

--- device.mk
+++ device.mk
@@ -101,7 +101,6 @@ PRODUCT_PACKAGES += \
     libOMX.TI.Video.Decoder \
     libOMX.TI.Video.encoder \
     libVendor_ti_omx \
-    gps.sholes \
     sensors.sholes \
     lights.sholes \
     Usb \
