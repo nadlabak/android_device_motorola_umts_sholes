32c7d5159d30594941421ee7ab53d4ba8e7d51b3
nadlabak <pavel@doshaska.net>
2012-02-07 19:49:54 +0100

    symlink the selected bp_nvm files on init
    Change-Id: Ia23a9e03b98efd9856d115a9f6dae942b0cdc7c2

---
+++ prebuilt/bin/init_bp_nvm.sh
@@ -0,0 +1,12 @@
+#!/system/bin/sh
+export PATH=/system/bin:/system/xbin:$PATH
+mount -o remount,rw /system
+BPVARIANT="b1b8"
+BPNVMPROP="/data/property/persist.sys.bp_nvm"
+BPNVMBASE="/system/etc/motorola/"
+if [ -e $BPNVMPROP]
+then
+    BPVARIANT=`cat $BPNVMPROP`
+fi
+ln -sf $BPNVMBASE$BPVARIANT/* $BPNVMBASE/bp_nvm_default
+mount -o remount,ro /system
