b8442d7007a7d95f1260128dd9b1e8b5465d72d0
nadlabak <pavel@doshaska.net>
2012-02-21 12:44:47 +0100

    Revert "use charge_only_mode and mot_boot_mode from device/motorola/common"
    This reverts commit 9c8f6b36025db99ea6254bae7614826376d22e22.

---
+++ charge_only_mode/Android.mk
@@ -0,0 +1,19 @@
+# Copyright 2005 The Android Open Source Project
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	alarm.c \
+	draw.c \
+	events.c \
+	hardware.c \
+	screen.c \
+	main.c
+
+LOCAL_STATIC_LIBRARIES := libunz libcutils libc
+
+LOCAL_C_INCLUDES := external/zlib
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE:= charge_only_mode
+
+include $(BUILD_EXECUTABLE)
