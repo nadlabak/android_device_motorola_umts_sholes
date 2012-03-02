faf14c856d4fc94593af6e60f09265265783f2d7
nadlabak <pavel@doshaska.net>
2012-03-03 00:06:49 +0100

    fix scripts
    Change-Id: I47b31774ebe3a61a1ee93bd806261684acad382a

--- prebuilt/bin/init_early_bind_mounts.sh
+++ prebuilt/bin/init_early_bind_mounts.sh
@@ -1,7 +1,7 @@
 #!/system/bin/sh
 #
 export PATH=/system/bin:/system/xbin:$PATH
-rm -f /data/logger/*
+busybox rm -f /data/logger/*
 mount -o bind /cache/logger /data/logger
 mount -o bind /pds/public /pds/public/battd
 mount -o bind /data/vendor/app /system/vendor/app
