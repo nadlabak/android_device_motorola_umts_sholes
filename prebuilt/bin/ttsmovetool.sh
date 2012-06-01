37659042124f36403f9f0648107e70f4f949ff83
nadlabak <pavel@doshaska.net>
2012-06-01 21:56:44 +0200

    ttsmovetool.sh: wipe dalvik-caches during install automatically
    Change-Id: Iabb1104a0923535dfce26e6580a94bb4600e5b3d
    TODO: rename the script

--- prebuilt/bin/ttsmovetool.sh
+++ prebuilt/bin/ttsmovetool.sh
@@ -1,8 +1,16 @@
 #!/sbin/sh
 
+# move tts files to /cdrom
 mount /cdrom
 rm -rf /cdrom/*
 cp -rp /system/tts/lang_pico /cdrom
 rm /system/tts/lang_pico/*
 ln -s /cdrom/lang_pico/* /system/tts/lang_pico
+
+# clear dex files
+rm /cache/dalvik-cache/*.dex
+mount /data
+rm /data/dalvik-cache/*.dex
+mount /sddata
+rm /sddata/dalvik-cache/*.dex
 
