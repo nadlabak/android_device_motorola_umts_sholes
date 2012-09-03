5c3f3f9f22086046fbc696a344f62e15c56abb8e
nadlabak <pavel@doshaska.net>
2012-09-04 00:20:58 +0200

    Revert "Revert "Enable usage of TLS register""
    This reverts commit c6c45a477e96888eb2c045193affc54b6d9e9425.

--- prebuilt/bin/ttsmovetool.sh
+++ prebuilt/bin/ttsmovetool.sh
@@ -7,6 +7,12 @@ cp -rp /system/tts/lang_pico /cdrom
 rm /system/tts/lang_pico/*
 ln -s /cdrom/lang_pico/* /system/tts/lang_pico
 
+# symlink sh to static mksh (in sbin) to use it before TLS-enable module is loaded
+# set the permissions for the static mksh
+ln -fs /system/sbin/mksh /system/bin/sh
+chown root.shell /system/sbin/mksh
+chmod 755 /system/sbin/mksh
+
 # clear dex files
 rm /cache/dalvik-cache/*.dex
 mount /data
