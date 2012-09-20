c496b2f287500ed747acd0548fc3595c57c8636a
nadlabak <pavel@doshaska.net>
2012-09-20 21:27:08 +0200

    usbd: add static build (for recovery)
    Change-Id: Ie56e4ec3fdcf75819e80fd652d3fb65f65c4722d

--- usbd/Android.mk
+++ usbd/Android.mk
@@ -10,4 +10,17 @@ LOCAL_MODULE := usbd
 
 LOCAL_SHARED_LIBRARIES := libcutils libc
 
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+        usbd.c
+
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := usbd_static
+
+LOCAL_STATIC_LIBRARIES := libcutils libc
+
 include $(BUILD_EXECUTABLE) 
