56364267d98bf29e2a91cf3b5f6c26a217d904fe
nadlabak <pavel@doshaska.net>
2012-10-08 02:03:43 +0200

    enable panic daemon, run ksm, log 2ndboot invocation
    Change-Id: Ic14bd87ed432fa07cb24b9130445cac76efd5062

--- prebuilt/bin/sh_hijack.sh
+++ prebuilt/bin/sh_hijack.sh
@@ -2,8 +2,8 @@
     echo 32 > /sys/bus/platform/devices/omapfb/graphics/fb0/bits_per_pixel
     echo 1 > /sys/bus/platform/devices/omapdss/display0/update_mode
     cat /data/2ndboot/2ndboot.fb > /dev/graphics/fb0
-    /system/bin/busybox_static insmod /system/lib/modules/2ndboot.ko
-    /system/bin/busybox_static mknod /dev/hbootctrl c 245 0
-    /system/bin/hbootuser /system/etc/hboot.cfg
+    /system/bin/busybox_static insmod /system/lib/modules/2ndboot.ko > /cache/2ndboot.log 2>&1
+    /system/bin/busybox_static mknod /dev/hbootctrl c 245 0 >> /cache/2ndboot.log 2>&1
+    /system/bin/hbootuser /system/etc/hboot.cfg >> /cache/2ndboot.log 2>&1
     /system/bin/busybox_static sleep 10
 
