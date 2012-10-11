359651fbf30f7012edbfa16cbf749de71e4d138e
nadlabak <pavel@doshaska.net>
2012-10-12 01:15:46 +0200

    copy bootinfo for after_2ndboot mot_boot_mode use
    Change-Id: Ie0818462c8efe3e0a2664d53474f0044b1749ef3

--- prebuilt/bin/sh_hijack.sh
+++ prebuilt/bin/sh_hijack.sh
@@ -2,6 +2,7 @@
     echo 32 > /sys/bus/platform/devices/omapfb/graphics/fb0/bits_per_pixel
     echo 1 > /sys/bus/platform/devices/omapdss/display0/update_mode
     /system/bin/busybox_static gunzip -c /etc/2ndboot/2ndboot.fb.gz > /dev/graphics/fb0
+    /system/bin/busybox_static cp -f /proc/bootinfo /data/bootinfo
     /system/bin/busybox_static insmod /system/lib/modules/2ndboot.ko > /cache/2ndboot.log 2>&1
     /system/bin/busybox_static mknod /dev/hbootctrl c 245 0 >> /cache/2ndboot.log 2>&1
     /system/bin/hbootuser /system/etc/hboot.cfg >> /cache/2ndboot.log 2>&1
